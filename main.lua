75505178f9e2d2a4388e93161b081d3719737d53
j <116266395+FragStudios@users.noreply.github.com>
2024-04-08 15:27:40 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,11 @@
-local pressAmount = 0
-local keys = { 30, 31 }
+local pressAmount = 0 -- Leave this at 0
+local timeout = 250 -- Adjust this if you like to reduce speedboosting/strafing even more
 
 local function breakStrafe(key, time)
     CreateThread(function()
         local finishTime = GetGameTimer() + time
         while finishTime > GetGameTimer() do
-            SetControlNormal(0, key, 1.0)
+            SetControlNormal(0, key, 1.0) -- This stops the speedboosting
             Wait(0)
         end
     end)
@@ -14,9 +14,9 @@ end
 CreateThread(function()
     while true do
         Wait(1000)
-        if pressAmount > 4 then
+        if pressAmount > 4 then -- Basically this checks if you are speedboosting
             local key = IsControlJustPressed(0, 30) and 30 or 31
-            breakStrafe(key, 250)
+            breakStrafe(key, timeout)
         end
         pressAmount = 0
     end
@@ -25,13 +25,10 @@ end)
 CreateThread(function()
     while true do
         local time = 1000
-        local playerPed = PlayerPedId()
-        if IsPlayerFreeAiming(PlayerId()) and not IsPedInAnyVehicle(playerPed) then
+        if IsPlayerFreeAiming(PlayerId()) and not IsPedInAnyVehicle(PlayerPedId()) then -- Can we actually aim in a vehicle? I guess so.. If driveby is enabled
             time = 0
-            for i=1, #keys do
-                if IsControlJustPressed(0, keys[i]) then
-                    pressAmount = pressAmount + 1
-                end
+            if IsControlJustPressed(0, 31) or IsControlJustPressed(0, 30) then -- If D or S key is pressed + 1 to press amount
+                pressAmount = pressAmount + 1
             end
         end
         Wait(time)
